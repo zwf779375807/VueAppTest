65bbe862df7b40fd5b45d3e1792e4099bb745b6b
zhangwf <zhangwf@elab-plus.com>
2019-04-22 11:18:56 +0800

    添加图片加载文件

--- src/compand/homecontainer/photoview/PhotoView.js
+++ src/compand/homecontainer/photoview/PhotoView.js
@@ -1,14 +1,41 @@
-
-import {Toast} from 'mint-ui'
+import {
+    Toast
+} from 'mint-ui'
 
 import mui from '../../../lib/js/mui.js'
 
-
-
 export default {
+
+    data() {
+        return {
+            cateGroyList: []
+        }
+    },
     mounted() {
         mui('.mui-scroll-wrapper').scroll({
-            deceleration: 0.0005 
-        });  
+            deceleration: 0.0005
+        });
+    },
+    created() {
+        this.getAllCategory()
+    },
+
+    methods: {
+        getAllCategory() {
+            this.$http.get('api/getimgcategory')
+                .then(result => {
+                    if (result.data.status === 0) {
+                        result.data.message.unshift({
+                            title: "全部",
+                            id: 0
+                        })
+                        this.cateGroyList = result.data.message
+                    } else {
+                        Toast('请求失败')
+                    }
+                }, err => {
+                    Toast('请求失败')
+                })
+        }
     },
 }
